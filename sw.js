9d4156d4d51f9ee081f802b4ae1951b7be7d8575
GlobalInvesting <69098699+GlobalInvesting@users.noreply.github.com>
2026-08-14 15:59:29 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
 // users always get fresh files after the next page load.
 // ═══════════════════════════════════════════════════════════════════
 
-const CACHE_VERSION = 'gi-v8.140.0';
+const CACHE_VERSION = 'gi-v8.141.0';
 const CACHE_STATIC  = `${CACHE_VERSION}-static`;
 const CACHE_DATA    = `${CACHE_VERSION}-data`;
 
