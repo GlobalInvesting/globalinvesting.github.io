77ded0b04a60e3b0872cbf58af54e79595610a0b
GlobalInvesting <69098699+GlobalInvesting@users.noreply.github.com>
2026-09-14 23:35:36 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_VERSION = 'gi-v8.491.3';
+const CACHE_VERSION = 'gi-v8.491.4';
 const CACHE_STATIC  = `${CACHE_VERSION}-static`;
 const CACHE_DATA    = `${CACHE_VERSION}-data`;
 
@@ -7,7 +7,7 @@ const STATIC_PRECACHE = [
   '/assets/dashboard.css?v=8.457.0',
   '/assets/command-bar.css?v=8.491.0',
   '/assets/dashboard.js?v=8.491.0',
-  '/assets/command-bar.js?v=8.491.3',
+  '/assets/command-bar.js?v=8.491.4',
   '/assets/gi-auth.js?v=1.7.8',
   '/assets/gi-overview.js?v=2.0.1',
   '/assets/fx-websocket.js?v=1.0.2',
