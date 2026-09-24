d96493a6c4fc03ccc0b40806b324b82697ded37d
GlobalInvesting <69098699+GlobalInvesting@users.noreply.github.com>
2026-09-24 20:58:46 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,18 @@
 
-const CACHE_VERSION = 'gi-v8.542.8';
+const CACHE_VERSION = 'gi-v8.542.9';
 const CACHE_STATIC  = `${CACHE_VERSION}-static`;
 const CACHE_DATA    = `${CACHE_VERSION}-data`;
 
 const STATIC_PRECACHE = [
   '/assets/dashboard.css?v=8.540.3',
   '/assets/command-bar.css?v=8.491.5',
-  '/assets/dashboard.js?v=8.540.3',
+  '/assets/pair-detail-beta.css?v=0.3.4',
+  '/assets/dashboard.js?v=8.541.0',
+  '/assets/pair-detail-beta.js?v=0.3.4',
   '/assets/command-bar.js?v=8.491.4',
   '/assets/gi-auth.js?v=1.7.8',
   '/assets/gi-overview.js?v=2.0.1',
-  '/assets/fx-websocket.js?v=1.0.4',
+  '/assets/fx-websocket.js?v=1.0.5',
   '/assets/cot-modal-chart.js?v=7.99.1',
   '/assets/cb-rates-modal.js?v=8.0.8',
   '/assets/real-carry-modal.js?v=2.7.11',
