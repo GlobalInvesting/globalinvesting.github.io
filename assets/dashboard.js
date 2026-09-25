ad615a611dcf0305c663fc2c74d8f3ffcc84ca7a
GlobalInvesting <69098699+GlobalInvesting@users.noreply.github.com>
2026-09-24 23:05:06 -0300

    Add files via upload

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -6615,7 +6615,7 @@ async function _renderLWChart(ohlcId, label) {
               lineWidth: 1,
               lineStyle: 2,
               axisLabelVisible: true,
-              axisLabelColor: 'rgba(0,0,0,0)',
+              axisLabelColor: _themeColor('--bg'),
               axisLabelTextColor: s.color,
               title: s.axisTitle || s.label,
             });
